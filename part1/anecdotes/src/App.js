e289309bcdef02351ed31118a3090d679c6e1366
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-01-18 09:57:15 -0300

    finished anecdotes and part 1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,36 +1,53 @@
 import { useState } from 'react'
 import Button from './Button'
-import Statistics from './Statistics '
+
 
 const App = () => {
   
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(Array(anecdotes.length).fill(0))
 
-  const handlerGood = () => {
-    setGood(good +1);
-    console.log(good);
-  }
-  const handlerNeutral = () => {
-    setNeutral(neutral +1);
-    console.log(neutral);
+  const handleSelect = () => {
+    let value = Math.floor(Math.random() * anecdotes.length)
+    setSelected(value)
+    console.log("value = ",value);
   }
   
-  const handlerBad = () => {
-    setBad(bad +1);
-    console.log(bad);
+  const HsetVote = () => {
+    let copy = [...votes]
+    copy[selected]+=1
+    console.log("copy = ",copy);
+    setVote(copy)
+  }
+
+  const getMostVoted = () => {
+    let value = votes.indexOf(Math.max(...votes))
+    console.log(value);
+    return anecdotes[value]
   }
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handler = {handlerGood} name = {'good'} />
-      <Button handler = {handlerNeutral} name = {'neutral'} />
-      <Button handler = {handlerBad} name = {'bad'} />
-      <h1>statistics</h1>
-      <Statistics good = {good} neutral = {neutral} bad = {bad} />
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br />
+      {`has ${votes[selected]} votes`}
+      <br />
+      <Button handler = {HsetVote} name = {'vote'} />
+      <Button handler = {handleSelect} name = {'next anecdote'} />
+      <h1>Anecdote with most votes</h1>
+      {getMostVoted()}
     </div>
   )
 }
--- part1/anecdotes/src/StatisticLine.js
+++
@@ -1,4 +0,0 @@
-const StatisticLine = ({text,value}) => {
-    return `${text} ${value}`
-}
-  export default StatisticLine 
