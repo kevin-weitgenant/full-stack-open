045820221af406cd9242a28ab0066f5ca880a863
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-01-21 11:32:06 -0300

    2.1 - 2.5

--- part2/Course information/src/Content.jsx
+++ part2/Course information/src/Content.jsx
@@ -2,7 +2,7 @@ const Content = ({parts}) => {
     console.log('content props = ', parts)
 
 
-    let paragraphs = parts.map(  (x) => [<p> {x.name} {x.exercises} </p>]);
+    let paragraphs = parts.map(  (x) => [<p key={x.id}> {x.name} {x.exercises} </p>]);
 
     return paragraphs
 }
