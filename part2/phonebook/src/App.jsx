9b8a5c8b99c3db1e79dd5c2da11df0c291127339
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-01-21 10:15:21 -0300

    exercicio 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,19 +2,28 @@ import { useState,useEffect } from 'react'
 import Filter from './Filter'
 import Form from './Form'
 import Persons from './Persons'
+import axios from 'axios'
 const App = () => {
   
+
+ 
+
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterString, setFilterString] = useState('')
   const [filteredNumbers, setfilteredNumbers] = useState('')
 
+
+  useEffect(() => {
+  axios
+  .get('http://localhost:3001/persons')
+  .then(response => {
+    setPersons(response.data)})
+  },[])
+  
 
   
   const getNames = () => persons.map(x => x.name)   //aux
