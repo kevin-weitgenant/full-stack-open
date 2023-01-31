e1336a095109a4ec8766abeff7f23bbce769cd3d
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-01-31 12:57:32 -0300

    fix api request (metric unit)

--- part2/countries/src/services/service.js
+++ part2/countries/src/services/service.js
@@ -4,7 +4,6 @@ let API_KEY = import.meta.env.VITE_SOME_KEY
 const baseUrl = 'https://restcountries.com/v3.1/all'
 
 const getCountries = () => axios.get(baseUrl)
-const getWeather = (lat,lon) => axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-
+const getWeather = (lat,lon) => axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`)
 
 export default {getCountries,getWeather}
