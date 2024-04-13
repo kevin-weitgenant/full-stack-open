41aa154e8647f37731c0b73eb135e794d380cd34
kevin-weitgenant <kevin.weitgenant@gmail.com>
2024-04-13 19:25:52 -0300

    4.1 and 4.2 again

--- part4/Blog list/controllers/blogs.js
+++ part4/Blog list/controllers/blogs.js
@@ -1,74 +1,23 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-
-blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-  })
-
-  blogsRouter.get('/:id', async (request, response) => {
-    console.log("chego aqui");
-    const id = request.params.id;
-    const blog = await Blog.findById(id)
-
-    if (response){
-      response.json(blog)
-    }
-    else{
-      response.status(404).send("not found")
-    }
-
-  })
-
-  blogsRouter.delete('/:id', async (request, response) => {
-    const id = request.params.id;
-    const result = await Blog.findByIdAndDelete(id)
-    console.log('result = ', result);
-    if (result){
-      response.status(200).send("sucessfully deleted")
-    }
-    
-    else{
-      response.status(404).send("resource not found")
-    }
-
-    
-  })
-  
-  blogsRouter.put('/:id', async (request, response) => {
-    const id = request.params.id;
-    const result = await Blog.findByIdAndUpdate(id,request.body)
-    console.log('result = ', result);
-
-    if (result){
-      response.status(200).send("sucessfully updated")
-    }
-    
-    else{
-      response.status(404).send("resource not found")
-    }
-
-    
-  })
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 
 
 
-  blogsRouter.post('/', async(request, response) => {
-    
-    let blog = request.body
 
-    if (blog?.author && blog?.url){
-      if (!blog?.likes){
-        blog['likes'] = 0
-      }
-  
-      blog = new Blog(request.body)
-  
-      const post = await blog.save()
-      response.status(201).json(post)
-    }
 
-    response.status(400).end()
-  })
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-module.exports = blogsRouter
+blogsRouter.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+
+module.exports = blogsRouter;
--- part4/Blog list/models/blog.js
+++ part4/Blog list/models/blog.js
@@ -1,30 +1,19 @@
 const mongoose = require('mongoose')
 
-
-
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-    // author: [
-    //   {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Post'
-    //   }
-    // ],
-  })
-
-blogSchema.set('toJSON', {
-transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-}
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
-
-
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-module.exports = Blog
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+module.exports = mongoose.model("Blog", blogSchema);
