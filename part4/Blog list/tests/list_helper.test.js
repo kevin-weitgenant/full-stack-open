f673a4041b4880ee82fd0e0ad0d0906da89117b0
kevin-weitgenant <kevin.weitgenant@gmail.com>
2024-04-14 21:37:23 -0300

    4.3 e 4.4

--- part4/Blog list/tests/list_helper.test.js
+++ part4/Blog list/tests/list_helper.test.js
@@ -8,3 +8,54 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+
+describe("total likes", () => {
+  const emptyList = [];
+
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const multipleBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
+    ...listWithOneBlog,
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0,
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes(emptyList);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog, equals the likes of that blog", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("of a list with multiple blogs is the sum of likes", () => {
+    const result = listHelper.totalLikes(multipleBlogs);
+    assert.strictEqual(result, 22); // 7 (React patterns) + 5 (Go To Statement) + 10 (First class tests)
+  });
+});
--- part4/Blog list/utils/list_helper.js
+++ part4/Blog list/utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
   return 1
 };
 
+
+
+function totalLikes(blogPosts) {
+  // Check if the array is empty and return 0 immediately if true
+  if (!blogPosts || blogPosts.length === 0) {
+    return 0;
+  }
+
+  // Use the reduce method to sum up the likes from each blog post
+  const total = blogPosts.reduce((sum, blog) => {
+    return sum + blog.likes;
+  }, 0);
+
+  return total;
+}
+
 module.exports = {
-  dummy,
+  dummy,totalLikes
 };
