07894e6fbd84fd8983616a8b2fd2b7cb543e8973
kevin-weitgenant <kevin.weitgenant@gmail.com>
2024-04-29 19:20:30 -0300

    6.5 and 6.6

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -4,7 +4,9 @@ import {voteAnecdote, createAnecdote} from './reducers/anecdoteReducer'
 
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => 
+  [...state].sort((a, b) => b.votes - a.votes)
+);
   const dispatch = useDispatch()
 
   const vote = (id) => {
